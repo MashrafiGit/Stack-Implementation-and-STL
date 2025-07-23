483c096015d2c48a206417596afecad2ba842a44
MashrafiGit <mdmasrafi902@gmail.com>
2025-07-23 19:47:29 +0600

    stack stl

---
+++ stack_stl.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    stack<int> s;
+
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        s.push(x);
+    }
+
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
